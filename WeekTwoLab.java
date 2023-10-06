97661f09f629c45286c9c036e39181eda1a98fa7
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-10-06 21:44:41 +0300

    WEEK2 LAB
    Scanner ile giriş alma.
    Kullanıcıdan değer alma ve girdiği değeri gösterme.
    Integer tutabileceğinden fazla değer aldırma.
    Print sırası önceliği

---
+++ WeekTwoLab.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class WeekTwoLab {
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        /*
+        System.out.println("PLEASE ENTER VALID FLOAT VALUE f1");
+        float f1 = sc.nextFloat();
+        System.out.println("F1 VALUE IS "+ f1);
+        System.out.println("PLEASE ENTER VALID INTEGER VALUE i1");
+        int i1 = sc.nextInt();
+        System.out.println("I1 VALUE IS "+ i1);
+        System.out.println("PLEASE ENTER VALID FLOAT VALUE f2");
+        float f2 = sc.nextFloat();
+        System.out.println("F2 VALUE IS "+ f2);
+        System.out.println("PLEASE ENTER VALID INTEGER VALUE i2");
+        int i2 = sc.nextInt();
+        System.out.println("I2 VALUE IS "+ i2);
+        */
+
+        /*
+        System.out.println("PLEASE ENTER 2 VALID INTEGER VALUE");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println("I1+I2 = "+ a+b);
+        System.out.println("I1*I2 = "+ a*b);
+        */
+
+        /*
+        int i = 5;
+        System.out.println(i++);
+        System.out.println(i);
+        System.out.println(++i);
+        System.out.println(i);
+        */
+        
+        /*
+        int i = 5;
+        System.out.println(i++);
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i);
+        */
+
+        /*
+        System.out.println(2);
+        System.out.println("2");
+        System.out.println(2);
+        System.out.println("2"+"2");
+        System.out.println(2+2);
+        */
+    }
+}
