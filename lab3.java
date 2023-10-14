72275dc37fcd3c37e3232d7d42c3e53e0b1b4147
CnBatuhan <138293421+CnBatuhan@users.noreply.github.com>
2023-10-14 17:46:45 +0300

    Week3 Lab 3

---
+++ lab3.java
@@ -0,0 +1,92 @@
+import java.util.Scanner;
+public class lab3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+        /*                 GİRİLEN 3 SAYIDAN EN BÜYÜK OLANI
+        System.out.println("LÜTFEN DEGER GİRİNİZ...");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+         if(a>b && a>c){
+                System.out.println("Büyük Sayı a Dır Ve a Degeri " + a);
+            }
+        else if(b>c){
+            System.out.println("Büyük Sayı b Dir Ve b Değeri " + b);
+        }
+        else{
+            System.out.println("Büyük Sayı c Dir Ve c Değeri " + c);
+        }
+        */
+        
+        
+        /*                  SWİTCH CASE BASİT HESAP MAKİNESİ
+        System.out.println("TOPLAM İÇİN 1 , ÇIKARMA İÇİN 2 , ÇARPIM 3 , BÖLÜM İÇİN 4 BASIN");
+        
+        int a = sc.nextInt();
+        switch(a){
+            case 1:
+                System.out.println("LÜTFEN TOPLAMAK İSTEDİGİNİZ DEĞERLERİ GİRİNİZ");
+                int z = sc.nextInt();
+                int x = sc.nextInt();
+                System.out.println("Toplamları = " + (z+x));
+                break;
+            case 2:
+                System.out.println("LÜTFEN ÇIKARMAK İSTEDİĞİNİZ DEĞERLERİ GİRİNİZ");
+                int c = sc.nextInt();
+                int v = sc.nextInt();
+                System.out.println("FARKLARI = " + (c-v));
+                break;
+            case 3:
+                System.out.println("LÜTFEN ÇARPAMK İSTEDİĞİNİZ DEĞERLERİ GİRİNİZ");
+                int b = sc.nextInt();
+                int n = sc.nextInt();
+                System.out.println("ÇARPIMLARI = " + (b*n));
+                break;
+            case 4:
+                System.out.println("LÜTFEN BÖLMEK İSTEDİĞİNİZ DEĞERLERİ GİRİNİZ");
+                float m = sc.nextFloat();
+                float s = sc.nextFloat();
+                System.out.println("BÖLÜMLERİ = " + (m/s));
+                break;
+        }*/
+
+        /*                   PIN İLE PROFİL SEÇİMİ
+        System.out.println("Lütfen Pın Giriniz...");
+        int a = sc.nextInt();
+        switch(a){
+            case 1234:
+                System.out.println("KONUK PROFİLİNE HOŞGELDİNİZ");
+                break;
+            case 1357:
+                System.out.println("YÖNETİCİ PROFİLİNE HOŞGELDİNİZ");
+                break;
+            case 2468:
+                System.out.println("ÇALIŞAN PROFİLİNE HOŞGELDİNİZ");
+                break;
+            case 1289:
+                System.out.println("GELİŞTİRİCİ PROFİLİNE HOŞGELDİNİZ");
+                break;
+        }*/
+        
+
+        /*                         YÖNETİCİ PİN                 
+        System.out.println("LÜTFEN PIN GİRİNİZ...");
+        int a = sc.nextInt();
+        switch(a){
+            case 1234:
+                System.out.println("İSİM = BATUHAN\nSOYİSİM = CAN");
+                break;
+            case 1357:
+                System.out.println("İSİM = ATİKE\nSOYISIM = CAN");
+                break;
+            case 123987456:
+                System.out.println("YÖNETİCİ PROFİLİNE HOŞGELDİNİZ.\n" +
+                                        "*** KULLANICI BİLGİLERİ ***\n" +
+                                        "1.KULLANICI: BATUHAN CAN\n"
+                                        + "2.KULLANICI: ATİKE CAN");
+                                        break;
+                                    }*/
+        }
+    }
